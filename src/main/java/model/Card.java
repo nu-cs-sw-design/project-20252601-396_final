42f0d2b270969c335c54952331d7cc1cfa9a9c53
yeeet0421 <nctucs.cs08@nycu.edu.tw>
2025-11-30 03:04:54 -0600

    CODE: refactor Card

--- src/main/java/model/Card.java
+++ src/main/java/model/Card.java
@@ -1,4 +1,29 @@
 package model;
 
+import model.exceptions.InvalidMoveException;
+
 public class Card {
+    private final CardType type;
+    private final CardAction action;
+
+    public Card(CardType type, CardAction action) {
+        this.type = type;
+        this.action = action;
+    }
+    public void performAction(GameContext ctx) {
+        if (action != null) {
+            action.execute(ctx);
+        } else {
+            throw new InvalidMoveException("Card: " + type.toString() + ", cannot perform action.");
+        }
+    }
+    public CardType getType() {
+        return type;
+    }
+
+    // for debugging and display
+    @Override
+    public String toString() {
+        return type.toString();
+    }
 }
