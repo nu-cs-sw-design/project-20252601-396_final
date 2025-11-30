288bcbb0516d4b13d70f4e63e95646e053836130
yeeet0421 <nctucs.cs08@nycu.edu.tw>
2025-11-30 04:02:39 -0600

    REFACTOR: refactor Deck's methods' functionalities

--- src/main/java/model/Deck.java
+++ src/main/java/model/Deck.java
@@ -1,4 +1,31 @@
 package model;
 
+import model.exceptions.InvalidMoveException;
+
+import java.util.List;
+
 public class Deck {
+    private List<Card> cards;
+    private static final String DRAW_FROM_EMPTY_DECK_EXCEPTION =
+            "Cannot draw card from empty deck.";
+
+    public Deck(){}
+    public void shuffle(){}
+    public Card draw(){
+        if (cards.isEmpty()) {
+            throw new InvalidMoveException(DRAW_FROM_EMPTY_DECK_EXCEPTION);
+        }
+        else {
+            return cards.remove(cards.size() - 1);
+        }
+    }
+    public void insertAt(Card card, int index){
+        cards.add(index, card);
+    }
+    public void swapTopAndBottom(){
+
+    }
+    public int getDeckSize(){
+        return cards.size();
+    }
 }
