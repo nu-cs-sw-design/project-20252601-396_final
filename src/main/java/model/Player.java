49d33d9574747a568087a567a7eef0cfd8f7073f
yeeet0421 <nctucs.cs08@nycu.edu.tw>
2025-11-30 03:37:33 -0600

    CODE: refactor Player

--- src/main/java/model/Player.java
+++ src/main/java/model/Player.java
@@ -1,4 +1,23 @@
 package model;
 
+import java.util.List;
+
 public class Player {
+    private final int id;
+    private List<Card> hand;
+    private boolean isDead = false;
+
+    public Player(int id) {
+        this.id = id;
+    }
+
+    public boolean hasDefuse(){
+        return false;
+    }
+    public void removeDefuse(){}
+    // did not handle when hand has no card etc.
+    public Card removeCard(int index){
+        return hand.remove(index);
+    }
+    public void addCard(Card card){}
 }
