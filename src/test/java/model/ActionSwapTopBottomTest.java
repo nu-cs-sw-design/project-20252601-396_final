f410825b5c789107315efbd36f669b6e4e98f493
yeeet0421 <nctucs.cs08@nycu.edu.tw>
2025-12-03 22:30:20 -0600

    TEST: added ActionSwapTopBottom tests

--- src/test/java/model/ActionSwapTopBottomTest.java
+++ src/test/java/model/ActionSwapTopBottomTest.java
@@ -1,4 +1,43 @@
 package model;
 
+import model.exceptions.NotEnoughCardsException;
+import org.easymock.EasyMock;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class ActionSwapTopBottomTest {
+    @Test
+    public void testExecuteSwapsWhenDeckHasEnoughCards() {
+        GameContext context = EasyMock.createMock(GameContext.class);
+        Deck deck = EasyMock.createMock(Deck.class);
+        ActionSwapTopBottom action = new ActionSwapTopBottom();
+
+        EasyMock.expect(context.getDeck()).andReturn(deck).anyTimes();
+        EasyMock.expect(deck.getDeckSize()).andReturn(2);
+        deck.swapTopAndBottom();
+        EasyMock.expectLastCall();
+        EasyMock.replay(context, deck);
+
+        action.execute(context);
+
+        EasyMock.verify(context, deck);
+    }
+
+    @Test
+    public void testExecuteThrowsExceptionWhenNotEnoughCards() {
+        GameContext context = EasyMock.createMock(GameContext.class);
+        Deck deck = EasyMock.createMock(Deck.class);
+        ActionSwapTopBottom action = new ActionSwapTopBottom();
+
+        EasyMock.expect(context.getDeck()).andStubReturn(deck);
+        EasyMock.expect(deck.getDeckSize()).andStubReturn(1);
+        EasyMock.replay(context, deck);
+
+        assertThrows(NotEnoughCardsException.class, () -> {
+            action.execute(context);
+        });
+
+        EasyMock.verify(context, deck);
+    }
 }
